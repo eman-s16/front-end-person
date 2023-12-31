e7a8623bcbc4af98f55bd0f6a231597f9b92f7ca
eman-s16 <projectm812@gmail.com>
2023-12-31 10:17:08 +0400

    s

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { EditPersonComponent } from './edit-person/edit-person.component';
+import { AddPersonComponent } from './add-person/add-person.component';
+import { ViewPersonComponent } from './view-person/view-person.component';
+import { PersonComponent } from './person/person.component';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent}
+    {path:'',component:HomeComponent, redirectTo: 'Home', pathMatch: 'full'},
+    
+  { path: 'Home', component: HomeComponent },
+  { path: 'list', component: PersonComponent },
+  { path: 'ViewPerson/:Id', component: ViewPersonComponent },
+  { path: 'AddPerson', component: AddPersonComponent },
+  { path: 'EditPerson/:Id', component: EditPersonComponent } 
 ];
