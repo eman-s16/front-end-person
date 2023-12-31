ea3a026cd60db392c3f76df382edf22ebcfc0555
eman-s16 <projectm812@gmail.com>
2023-12-31 10:00:35 +0400

    a

---
+++ src/app/view-person/view-person.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-person',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-person.component.html',
+  styleUrl: './view-person.component.css'
+})
+export class ViewPersonComponent {
+
+}
